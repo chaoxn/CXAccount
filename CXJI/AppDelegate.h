753d4e6acba9af14b913206a65f613557bcf57ee
chaoxn <chaoxn@gmail.com>
2016-10-13 15:06:35 +0800

    Initial Commit

---
+++ CXJI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CXJI
+//
+//  Created by fizz on 16/10/13.
+//  Copyright © 2016年 chaox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
